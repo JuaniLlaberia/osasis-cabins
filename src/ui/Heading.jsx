789b0f2e57591298bc6c2e99381de269f590ebec
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-07-16 18:32:54 -0300

    App structure

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,30 @@
+import { css, styled } from 'styled-components';
+
+// const test = css`
+//   text-align: center;
+//   font-size: ${10 > 5 ? '30px' : '5px'};
+// `;
+
+const Heading = styled.h1`
+  ${props =>
+    props.as === 'h1' &&
+    css`
+      font-weight: 600;
+      font-size: 3rem;
+    `}
+  ${props =>
+    props.as === 'h2' &&
+    css`
+      font-weight: 600;
+      font-size: 2rem;
+    `}
+  ${props =>
+    props.as === 'h3' &&
+    css`
+      font-weight: 500;
+      font-size: 1rem;
+    `}
+  line-height: 1.4;
+`;
+
+export default Heading;
---
+++ src/ui/Input.jsx
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+const Input = styled.input`
+  background-color: var(--color-gray-0);
+  border: 1px solid var(--color-gray-300);
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-md);
+  padding: 0.8rem 1.2rem;
+`;
+
+export default Input;
