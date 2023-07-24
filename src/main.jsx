8d14ba5915cc0f490c4f04909cfa13624da8b8b7
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-07-24 10:04:55 -0300

    Added Error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { DarkModeProvider } from './context/DarkModeConext.jsx';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from './ui/ErrorFallback.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DarkModeProvider>
-      <App />
+      <ErrorBoundary //To handle error on react rendering (just on that, the rest of errors we need to handle them on each place)
+        FallbackComponent={ErrorFallback} //Component to render instead of white screen
+        onReset={() => window.location.replace('/')} //function we can pass
+      >
+        <App />
+      </ErrorBoundary>
     </DarkModeProvider>
   </React.StrictMode>
 );
